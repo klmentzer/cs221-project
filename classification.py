6acb8b2eaca69109620d80a87c6732cfe401ab69
Bihan Jiang <bihanjiang@DN0a1faa7e.SUNet>
2019-05-31 01:07:14 -0700

    added inceptionv4

--- classification.py
+++ classification.py
@@ -8,33 +8,42 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Flatten, Dense, Dropout
 from keras import optimizers
+import sys
+sys.path.insert(0, 'kerasinceptionV4master/inception_v4')
+from kerasinceptionV4master.inception_v4 import create_model
 import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
+#with device('/gpu:0'):
 parent_dir =os.path.join( os.getcwd(), '../asl-alphabet/asl_alphabet_train/')
 test_dir = parent_dir+"test"
 val_dir = parent_dir+"val"
 train_dir = parent_dir+"train"
 
-classifier = Sequential()
+classifier = create_model(num_classes=29, dropout_prob=0.2, weights=None, include_top=True)#Sequential()
+for layer in classifer.layers[:9*len(classifier.layers)//10]:
+    layer.trainable = False
 
-classifier.add(Conv2D(32, (5, 5), input_shape = (200, 200, 3), activation='relu'))
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-
-classifier.add(Conv2D(32, (3, 3), input_shape = (200, 200, 3), activation='relu'))
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-
-classifier.add(Conv2D(64, (3, 3), activation='relu'))
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-
-classifier.add(Flatten())
-classifier.add(Dense(64))
-classifier.add(Activation('relu'))
-classifier.add(Dropout(0.5))
-classifier.add(Dense(29))
-classifier.add(Activation('softmax'))
+print(len(classifier.layers))
+#
+# classifier.add(Conv2D(32, (5, 5), input_shape = (200, 200, 3), activation='relu'))
+# classifier.add(MaxPooling2D(pool_size=(2, 2)))
+#
+# classifier.add(Conv2D(32, (3, 3), input_shape = (200, 200, 3), activation='relu'))
+# classifier.add(MaxPooling2D(pool_size=(2, 2)))
+#
+# classifier.add(Conv2D(64, (3, 3), activation='relu'))
+# classifier.add(MaxPooling2D(pool_size=(2, 2)))
+#
+# classifier.add(Flatten())
+# classifier.add(Dense(64))
+# classifier.add(Activation('relu'))
+# classifier.add(Dropout(0.5))
+# classifier.add(Dense(29))
+# classifier.add(Activation('softmax'))
 
 # rmsprop = optimizers.RMSprop(lr=0.01, rho=0.7, epsilon=1e-8, decay=0.0)
-
+#
 classifier.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
@@ -60,20 +69,20 @@ validation_generator = test_datagen.flow_from_directory(
         class_mode='categorical')
 print(classifier.summary())
 
-classifier.fit_generator(train_generator, 
-        steps_per_epoch=2000, 
-        epochs=50, 
-        validation_data=validation_generator, 
+classifier.fit_generator(train_generator,
+        steps_per_epoch=2000,
+        epochs=50,
+        validation_data=validation_generator,
         validation_steps=800)
 
 classifier.save_weights('first_try.h5')
-# layers.Input(name='the_input', shape=(200,200), dtype='float32')
-# labels = layers.Input(name='the_labels',
-#                    shape=[img_gen.absolute_max_string_len], dtype='float32')
-#
-# model.fit_generator(
-#         train_generator,
-#         steps_per_epoch=2000,
-#         epochs=50,
-#         validation_data=validation_generator,
-#         validation_steps=800)
+    # layers.Input(name='the_input', shape=(200,200), dtype='float32')
+    # labels = layers.Input(name='the_labels',
+    #                    shape=[img_gen.absolute_max_string_len], dtype='float32')
+    #
+    # model.fit_generator(
+    #         train_generator,
+    #         steps_per_epoch=2000,
+    #         epochs=50,
+    #         validation_data=validation_generator,
+    #         validation_steps=800)
---
+++ preprocess.py
@@ -0,0 +1,26 @@
+import tensorflow
+from tensorflow.contrib import keras
+
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+
+datagen = ImageDataGenerator(
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
+img = load_img('../asl_alphabet_train/A/A1.jpg')  # this is a PIL image
+x = img_to_array(img)  # this is a Numpy array with shape (300, 300, 3)
+x = x.reshape((1,) + x.shape)  # this is a Numpy array with shape (1, 300, 300, 3)
+x.shape
+
+# the .flow() command below generates batches of randomly transformed images
+# and saves the results to the `preview/` directory
+i = 0
+for batch in datagen.flow(x, batch_size=1,
+                         save_to_dir='preview', save_prefix='el', save_format='jpeg'):
+    i += 1
+    if i > 20:
+        break  # otherwise the generator would loop indefinitely
