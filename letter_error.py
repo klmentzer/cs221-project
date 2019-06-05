16e8effb1a4cdfac432dc9c4e7ba45ab131220f1
Kaleigh Mentzer <kmentzer@stanford.edu>
2019-06-05 15:34:15 -0700

    error by letter plots for poster

--- letter_error.py
+++ letter_error.py
@@ -10,7 +10,7 @@ from keras.layers import Activation, Flatten, Dense, Dropout
 from keras import optimizers
 from segment_data import get_category_from_filename
 from glob import glob
-
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 classifier = Sequential()
 
@@ -40,38 +40,95 @@ classifier.compile(loss='categorical_crossentropy',
 parent_dir =parent_dir =os.path.join( os.getcwd(), '../asl-alphabet/asl_alphabet_train/val')
 letter_folders = list(set(os.listdir(parent_dir)))
 letter_folders = [s for s in letter_folders if s[0]!= '.' and s not in ['test','val','train']]
+letter_folders.sort()
 acc_dict = {}
 results = []
 j = 0
-for letter in letter_folders:
-    print(letter)
-    num = np.random.choice(range(301,601))
-    filename = os.path.join( os.getcwd(), '../asl-alphabet/asl_alphabet_train/val',letter,str(letter)+str(num)+'.jpg')
-    img = cv2.imread(filename)
-    img = img.reshape(-1,200, 200, 3)
-    preds = classifier.predict(img)
-    print(preds)
 
-    parent_dir =os.path.join( os.getcwd(), '../asl-alphabet/asl_alphabet_train/val',letter,'*')
-    letter_imgs = glob(parent_dir)
-    for i in range(len(letter_imgs)):
-        img = cv2.imread(letter_imgs[i])
-        to_test[i,:,:,:] = img
+# for k in range(3):
+#     j=0
+#     for letter in letter_folders:
+#         print(letter)
+#         num = np.random.choice(range(301,601))
+#         filename = os.path.join( os.getcwd(), '../asl-alphabet/asl_alphabet_train/val',letter,str(letter)+str(num)+'.jpg')
+#         img = cv2.imread(filename)
+#         img = img.reshape(-1,200, 200, 3)
+#         preds = classifier.predict(img)
+#         print(preds)
+#
+#         parent_dir =os.path.join( os.getcwd(), '../asl-alphabet/asl_alphabet_train/val',letter,'*')
+#         letter_imgs = glob(parent_dir)
+#         for i in range(len(letter_imgs)):
+#             img = cv2.imread(letter_imgs[i])
+#             to_test[i,:,:,:] = img
+#
+#         expected = np.tile(preds,300)
+#         expected = expected.reshape((300,29))
+#         # expected = np.zeros((300,29))
+#         # expected[:,5] = np.ones(300)
+#         preds = classifier.evaluate(to_test,expected)
+#         print(preds)
+#         print(classifier.metrics_names)
+#         #acc_dict[letter] = preds[1]
+#         if k == 0:
+#             results.append([letter,preds[1]])
+#         else:
+#             results[j].append(preds[1])
+#         j +=1
+#
+# print(acc_dict)
+# for result in results:
+#     print(result)
 
-    expected = np.tile(preds,300)
-    expected = expected.reshape((300,29))
-    # expected = np.zeros((300,29))
-    # expected[:,5] = np.ones(300)
-    preds = classifier.evaluate(to_test,expected)
-    print(preds)
-    print(classifier.metrics_names)
-    acc_dict[letter] = preds[1]
-    results.append([letter,preds[1]])
-    j +=1
+results = [['A', 0.8633333341280619, 0.8633333341280619, 0.8633333341280619],
+['B', 0.39, 0.39, 0.27],
+['C', 0.97, 0.97, 0.97],
+['D', 0.5533333325386047, 0.5533333325386047, 0.44333333293596905],
+['E', 0.42333333353201547, 0.576666665871938, 0.42333333353201547],
+['F', 0.9200000007947285, 0.03333333333333333, 0.9200000007947285],
+['G', 0.5633333335320155, 0.5633333335320155, 0.4366666658719381],
+['H', 0.9666666658719381, 0.9666666658719381, 0.9666666658719381],
+['I', 0.74, 0.74, 0.74],
+['J', 0.9933333333333333, 0.9933333333333333, 0.9933333333333333],
+['K', 0.5533333333333333, 0.43, 0.43],
+['L', 0.8933333333333333, 0.8933333333333333, 0.10666666666666667],
+['M', 0.573333334128062, 0.573333334128062, 0.31333333333333335],
+['N', 0.82, 0.82, 0.82],
+['O', 0.7766666674613952, 0.22333333373069764, 0.22333333373069764],
+['P', 0.5400000007947287, 0.4600000003973643, 0.4600000003973643],
+['Q', 0.38666666666666666, 0.6133333333333333, 0.38666666666666666],
+['R', 0.6566666674613952, 0.6566666674613952, 0.6566666674613952],
+['S', 0.9866666666666667, 0.9866666666666667, 0.9866666666666667],
+['T', 0.88, 0.04666666666666667, 0.88],
+['U', 0.6766666674613953, 0.2, 0.06],
+['V', 0.7899999992052714, 0.20666666626930236, 0.7899999992052714],
+['W', 0.6899999992052714, 0.6899999992052714, 0.6899999992052714],
+['X', 0.8966666666666666, 0.8966666666666666, 0.8966666666666666],
+['Y', 0.8933333333333333, 0.8933333333333333, 0.8933333333333333],
+['Z', 0.8966666666666666, 0.8966666666666666, 0.8966666666666666],
+['del', 0.7766666658719381, 0.7766666658719381, 0.7766666658719381],
+['nothing', 0.07, 0.6666666666666666, 0.6666666666666666],
+['space', 0.556666665871938, 0.16333333333333333, 0.556666665871938]]
 
-print(acc_dict)
-for result in results:
-    print(result)
+
+
+
+labels = [x[0] for x in results]
+acc = [max(x[1:]) for x in results]
+
+
+y_pos = np.arange(len(labels))
+
+fig = plt.figure()
+plt.bar(y_pos, acc, align='center', alpha=0.5)
+plt.xticks(y_pos, labels,rotation='vertical')
+plt.xlabel('Letter')
+plt.ylabel('Accuracy')
+plt.title("Classification Accuracy by Letter")
+fig.tight_layout()
+
+
+plt.show()
 
 # filename = os.path.join( os.getcwd(), '../asl-alphabet/asl_alphabet_train/val/A/A301.jpg')
 # img = cv2.imread(filename)
@@ -79,8 +136,8 @@ for result in results:
 # preds = classifier.predict(img)
 # print(preds)
 
-parent_dir =os.path.join( os.getcwd(), '../asl-alphabet/asl_alphabet_train/val/*')
-class_names = glob(parent_dir)
+# parent_dir =os.path.join( os.getcwd(), '../asl-alphabet/asl_alphabet_train/val/*')
+# class_names = glob(parent_dir)
 
 # print(class_names)
 
