e26e9c71e79a357b1b841e4a4f29f4ddf70f7d40
Madeline Liao <madeline.m.liao@gmail.com>
2019-06-07 11:53:28 -0700

    debugging

--- classification.py
+++ classification.py
@@ -75,7 +75,7 @@ validation_generator = test_datagen.flow_from_directory(
 # print(classifier.summary())
 
 classifier.fit_generator(train_generator,
-        steps_per_epoch=2000,
+        steps_per_epoch=2000, max_q_size=3, 
         epochs=5,
         validation_data=validation_generator,
         validation_steps=800)
