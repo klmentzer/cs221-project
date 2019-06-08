979e81b007f800d34f82c959b11e6a0ac78a2b37
Madeline Liao <madeline.m.liao@gmail.com>
2019-06-08 03:07:38 -0700

    gpu stuff

--- resnet_classification.py
+++ resnet_classification.py
@@ -15,8 +15,8 @@ import os
 
 
 def main():
-    config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
-    sess = tf.Session(config=config)
+    # config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
+    # sess = tf.Session(config=config)
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
     with tf.device('/device:GPU:0'):
         INPUT_SHAPE = (299, 299, 3)
