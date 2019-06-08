8e8fdaf66f3498f0c27b74d3a3a007dcde952990
Madeline Liao <madeline.m.liao@gmail.com>
2019-06-08 03:15:02 -0700

    tf stuff

--- resnet_classification.py
+++ resnet_classification.py
@@ -1,13 +1,14 @@
 '''
 First stab at loading data into Keras
 '''
-import tensorflow as tf
-from tf.keras.preprocessing import image
-from tf.keras.models import Sequential
-from tf.keras.layers import Conv2D, MaxPooling2D
-from tf.keras.layers import Activation, Flatten, Dense, Dropout
-from tf.keras import optimizers
+
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Activation, Flatten, Dense, Dropout
+from tensorflow.keras import optimizers
 from resnet import ResNet50
+import tensorflow as tf
 import sys
 from kerasinceptionV4master.inception_v4 import create_model
 import os
