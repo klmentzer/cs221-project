5016aad2e231defdf5f570b8f433e9a0ebbb2481
Madeline Liao <madeline.m.liao@gmail.com>
2019-06-08 01:53:02 -0700

    changed batch size

--- resnet_classification.py
+++ resnet_classification.py
@@ -43,13 +43,13 @@ def main():
     train_generator = train_datagen.flow_from_directory(
             train_dir,
             target_size=(299,299),
-            batch_size=32,
+            batch_size=1,
             class_mode="categorical")
 
     validation_generator = test_datagen.flow_from_directory(
             val_dir,
             target_size=(299,299),
-            batch_size=32,
+            batch_size=1,
             class_mode='categorical')
 
 
