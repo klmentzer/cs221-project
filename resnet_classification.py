f86665b730e889cadd3561a31bb4f68baba80ba8
Madeline Liao <madeline.m.liao@gmail.com>
2019-06-08 04:14:06 -0700

    using keras resnet

--- resnet_classification.py
+++ resnet_classification.py
@@ -1,13 +1,18 @@
 '''
-First stab at loading data into Keras
+Classification with ResNet
 '''
 
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
 from tensorflow.keras.layers import Activation, Flatten, Dense, Dropout
 from tensorflow.keras import optimizers
-from resnet import ResNet50
+from tensorflow.keras.models import Model
+# from resnet import ResNet50
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
 import tensorflow as tf
 import sys
 from kerasinceptionV4master.inception_v4 import create_model
@@ -15,8 +20,6 @@ import os
 
 
 def main():
-    # config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
-    # sess = tf.Session(config=config)
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
     with tf.device('/device:GPU:0'):
         INPUT_SHAPE = (299, 299, 3)
@@ -28,13 +31,33 @@ def main():
         val_dir = parent_dir + "val"
         train_dir = parent_dir + "train"
 
-        resnet = ResNet50(classes=NUM_CLASSES, input_shape=INPUT_SHAPE)
-        model = resnet.build()
+        # resnet = ResNet50(classes=NUM_CLASSES, input_shape=INPUT_SHAPE)
+        # model = resnet.build()
+        base_model = ResNet50(include_top=False, weights='imagenet', input_tensor=None, input_shape=(299, 299, 3), pooling=None, classes=1000)
 
+        x = base_model.output
+        x = GlobalAveragePooling2D()(x)
+        x = Dropout(0.7)(x)
+        predictions = Dense(NUM_CLASSES, activation= 'softmax')(x)
+        model = Model(inputs = base_model.input, outputs = predictions)
+        # model = Conv2D(32, (5, 5), input_shape = (299, 299, 3), activation='relu')(model)
+        # model = MaxPooling2D(pool_size=(2, 2))(model)
+        
+        # model = Conv2D(32, (3, 3), input_shape = (299, 299, 3), activation='relu')(model)
+        # model = MaxPooling2D(pool_size=(2, 2))(model)
+        
+        # model = Conv2D(64, (3, 3), activation='relu')(model)
+        # model = MaxPooling2D(pool_size=(2, 2))(model)
+        
+        # model = Flatten()(model)
+        # model = Dense(64)(model)
+        # model = Activation('relu')(model)
+        # model = Dropout(0.5)(model)
+        # model = Dense(29)(model)
+        # model = Activation('softmax')(model)
         model.compile(loss='categorical_crossentropy',
                     optimizer='rmsprop',
                     metrics=['accuracy'])
-
         # create tool to input image data
         train_datagen = image.ImageDataGenerator(
                 rescale=1./255,
